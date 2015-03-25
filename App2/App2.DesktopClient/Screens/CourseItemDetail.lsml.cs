9ad8a78b726d944989f09f015562d3ae35e3982f
Lina92 <Kotenok_bugulma@mail.ru>
2015-03-25 20:32:04 +0300

    6

---
+++ App2/App2.DesktopClient/Screens/CourseItemDetail.lsml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CourseItemDetail
+    {
+        partial void CourseItem_Loaded(bool succeeded)
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.CourseItem);
+        }
+
+        partial void CourseItem_Changed()
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.CourseItem);
+        }
+
+        partial void CourseItemDetail_Saved()
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.CourseItem);
+        }
+    }
+}
---
+++ App2/App2.DesktopClient/Screens/Schedule_trainerItemDetail.lsml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class Schedule_trainerItemDetail
+    {
+        partial void Schedule_trainerItem_Loaded(bool succeeded)
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.Schedule_trainerItem);
+        }
+
+        partial void Schedule_trainerItem_Changed()
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.Schedule_trainerItem);
+        }
+
+        partial void Schedule_trainerItemDetail_Saved()
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.Schedule_trainerItem);
+        }
+    }
+}
