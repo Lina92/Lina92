8fc7ccc400c2f1fbb170f986518b9c8208f8c47f
Lina92 <Kotenok_bugulma@mail.ru>
2015-03-28 22:00:12 +0300

    1

---
+++ App2/App2.DesktopClient/Screens/CreateNewSchedule_trainerItem.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewSchedule_trainerItem
+    {
+        partial void CreateNewSchedule_trainerItem_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Добавьте сюда свой код.
+            this.Schedule_trainerItemProperty = new Schedule_trainerItem();
+        }
+
+        partial void CreateNewSchedule_trainerItem_Saved()
+        {
+            // Добавьте сюда свой код.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.Schedule_trainerItemProperty);
+        }
+    }
+}
